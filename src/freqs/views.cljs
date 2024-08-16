913b92ca93b4be1a3eb0b0ef4fd9ae6162781a1d
Eoin Carney <eoincarney0@gmail.com>
2024-08-16 12:37:31 +0100

    added copy to clipboard functionality

--- src/freqs/views.cljs
+++ src/freqs/views.cljs
@@ -29,12 +29,63 @@
 (defn stat-component [title value]
   [:div {:class "flex justify-between w-64 py-2"}
    [:p {:class "font-bold pr-5"} title]
-   [:p {:class "text-fuchsia-700"} value]])
+   [:p {:class "text-fuchsia-700 pr-5"} value]])
 
-(defn words-component [word count]
-  [:div {:class "flex justify-between w-48 py-2"}
-   [:p word]
-   [:p count]])
+(defn copy-to-clipboard [val]
+  (let [el (js/document.createElement "textarea")]
+    (set! (.-value el) val)
+    (.appendChild js/document.body el)
+    (.select el)
+    (js/document.execCommand "copy")
+    (.removeChild js/document.body el)))
+
+(defn data->csv-string [results]
+  (reduce (fn [csv [word fq]]
+            (if (empty? csv)
+              (str "Word,Count\r\n"
+                   (str word "," fq))
+              (str csv "\r\n" (str word "," fq))))
+          ""
+          results))
+
+;; TODO get tooltip working
+;; TODO consider wheather to sort here and whether to respect other filters (e.g., ignore stopwords)
+(defn copy-table-data []
+  (let [data @(re-frame/subscribe [::subs/results])
+        csv (data->csv-string data)]
+    [:div
+     [:button
+      {:href "javascript:"
+       :on-click #(do (.stopPropagation %)
+                      (copy-to-clipboard csv))
+       :data-tooltip-target "copy-tooltip"}
+      [:span {:class "far fa-copy"}]]
+     [:div
+      {:id "copy-tooltip"
+       :role "tooltip"
+       :class "absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"}
+      "Tooltip"]]))
+
+;; (defn copyable-table [value]
+;;   [:a.tooltip.is-tooltip-left
+;;    {:href "javascript:"
+;;     :data-tooltip "Click to Copy"
+;;     :on-click #(do (.stopPropagation %)
+;;                    (copy-to-clipboard value))}
+;;    [:div "Click to Copy"]])
+
+
+(defn word-frequencies-table [results]
+  (into [:table {:class "w-full text-sm text-left rtl:text-right text-gray-500"}]
+        [[:thead {:class "text-xs text-gray-900 uppercase bg-gray-50"}
+          [:tr {:style {:user-select "text"}}
+           [:th {:class "px-6 py-3"} "Word"]
+           [:th {:class "px-6 py-3"}"Word Count"]]]
+         (into [:tbody]
+               (map (fn [[word fq]] [:tr {:class "bg-white"}
+                                     [:td {:class "px-6 py-2"} word]
+                                     [:td {:class "px-6 py-2"} fq]])
+                    (reverse (sort-by val results))))]))
 
 
 (defn results-display [results]
@@ -45,25 +96,23 @@
     [:div
      [results-filters]
      [:hr {:class "my-6"}]
-     [:div {:class "pt-4"}
+     [:div
       [:h2 {:class "text-2xl font-bold py-2"} "Stats"]
       [stat-component "Number of Words" (f/word-count results)]
       [stat-component "Number of Unique Words" (f/unique-wrods results)]
       [:p {:class "font-bold pr-5 py-2"} "Longest Words:"]
       (into
-       [:div {:class "flex py-2"}]
+       [:div {:class "flex flex-wrap py-2"}]
        (for [w (f/longest-words results 5)
              :let [[word length] w]]
-         [:p {:class "bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"}
+         [:p {:class "bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 m-1 rounded dark:bg-blue-900 dark:text-blue-300"}
           (str word " (" length ")")]))]
      [:hr {:class "my-6"}]
-
      [:div
-      [:h2 {:class "text-2xl font-bold pb-4"} "Word Frequencies"]
-      (into [:div]
-            (for [item (reverse (sort-by val results))
-                  :let [[word fq] item]]
-              [words-component word fq]))]]))
+      [:div {:class "flex justify-between"}
+       [:h2 {:class "text-2xl font-bold pb-4"} "Word Frequencies"]
+       [copy-table-data]]
+      [word-frequencies-table results]]]))
 
 ;; some options to include
 ;; - ignore case (true/false)
@@ -76,13 +125,14 @@
 ;; - TODO average word length
 
 
+
 (defn main-panel []
   (let [results @(re-frame/subscribe [::subs/results])]
-    [:div {:class "flex flex-col justify-center mx-64"}
+    [:div {:class "flex flex-col justify-center max-w-prose m-auto"}
      [:div
       [:h1 {:class "pt-5 mb-4 text-2xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl"}
        [:span {:class "text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"} "Word Frequency "] "Calculator"]]
      [input-form]
      (when results
-       [:div {:class "mt-5 p-5 border border-indigo-600"}
+       [:div {:class "my-5 p-5 sm:border sm:border-indigo-600"}
         [results-display results]])]))
